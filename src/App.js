d22328cab127fb472a6ae989b282e879638339a8
nader2356 <naderabdessaied8@gmail.com>
2023-07-20 18:06:43 +0200

    call api signup from backend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Component/Header';
 import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div >
-    
+     <Header/>
      <main className='pt-16'>
       <Outlet/>
      </main>
--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Signup from './page/SignUp';
+import Home from './page/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const router= createBrowserRouter(
   createRoutesFromElements(
     <Route path = '/' element ={<App/>}>
+     <Route index element={<Home />} />
+      
+        <Route path="signup" element={<Signup />} />
     </Route>
   )
 )
