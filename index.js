3d410e70b523c867218405961336b866d8c7761f
nader2356 <naderabdessaied8@gmail.com>
2023-07-25 23:00:19 +0200

    create userShema

--- index.js
+++ index.js
@@ -19,7 +19,20 @@ mongoose
   .then(() => console.log("Connect to Databse"))
   .catch((err) => console.log(err));
 
-
+  const userSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: {
+      type: String,
+      unique: true,
+    },
+    password: String,
+    confirmPassword: String,
+    image: String,
+  });
+  
+  //
+  const userModel = mongoose.model("user", userSchema);
   
 
 
